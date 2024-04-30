a10aa5cb286eb959f22dd6730913dde198b668fe
Kevin Y. Kim <kevinyckim33@gmail.com>
2024-04-30 17:03:05 -0500

    Finish Create Reducer Exercise

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,12 +1,29 @@
+import { useReducer } from 'react';
+import {
+  reduxToolKitCounterReducer,
+  classicReducer,
+  decrement,
+  increment,
+  reset,
+} from '../features/counter-reducer';
+
 const Counter = () => {
+  // point made, reducers made via reduxToolkit's createReducer/createAction
+  // are in the end exactly same as the ol' reducer you've been using since Flatiron
+  // one could say, the syntax is cleaner with reduxToolkit?
+  const [state, dispatch] = useReducer(reduxToolKitCounterReducer, {
+    count: 0,
+  }); // react makes you put in a default initial state here unfortunately
+  // const [state, dispatch] = useReducer(classicReducer, { count: 0 }); // react makes you put in a default initial state here unfortunately
+
   return (
-    <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
+    <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => dispatch(decrement(1))}>➖ Decrement</button>
+        <button onClick={() => dispatch(reset())}>🔁 Reset</button>
+        <button onClick={() => dispatch(increment(1))}>➕ Increment</button>
       </div>
       <div>
         <form onSubmit={(e) => e.preventDefault()}>
--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -1,20 +1,60 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 type CounterState = { count: number };
 type CounterAction =
-  | { type: 'INCREMENT' | 'DECREMENT'; payload: number }
-  | { type: 'RESET' };
+  | ReturnType<typeof increment>
+  | ReturnType<typeof decrement>
+  | ReturnType<typeof reset>;
 
-export const reducer = (state: CounterState, action: CounterAction) => {
-  if (action.type === 'INCREMENT') {
+// replaces
+// type CounterAction =
+//   | { type: 'INCREMENT' | 'DECREMENT'; payload: number }
+//   | { type: 'RESET' };
+
+export const increment = createAction('INCREMENT', (amount: number) => {
+  return { payload: amount };
+});
+
+export const decrement = createAction('DECREMENT', (amount: number) => {
+  return { payload: amount };
+});
+
+export const reset = createAction('RESET');
+
+// this is actually just a regular reducer!
+export const reduxToolKitCounterReducer = createReducer(
+  { count: 0 },
+  (builder) => {
+    builder.addCase(increment, (state, action) => {
+      state.count += action.payload; // Immer Writeable Draft; gave copy of current state; mutate all you want
+      // it'll do all the readable operations on your behalf
+      // pretend they're mutable, but it'll be immutable
+
+      // also you didn't have to return anything!
+    });
+
+    builder.addCase(decrement, (state, action) => {
+      state.count -= action.payload;
+    });
+
+    builder.addCase(reset, (state) => {
+      state.count = 0;
+    });
+  },
+);
+
+// the following is exactly the same as above
+// but no typing necessary
+export const classicReducer = (state: CounterState, action: CounterAction) => {
+  if (action.type === increment.type) {
     return { count: state.count + action.payload };
   }
 
-  if (action.type === 'DECREMENT') {
+  if (action.type === decrement.type) {
     return { count: state.count - action.payload };
   }
 
-  if (action.type === 'RESET') {
+  if (action.type === reset.type) {
     return { count: 0 };
   }
 
